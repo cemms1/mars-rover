2c4743db1f69a23b7f226a52a24f5d57523eb4a5
cemms1 <charlotteemms@live.co.uk>
2021-05-26 15:22:15 +0100

    make args easier to input

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import marsRover from './src/index.js'
 
-const results = marsRover('4 8', '(2, 3, E) LFRFF', '(0, 2, N) FFLFRFF')
+const grid = '4 8'
+const robots = ['(2, 3, N) FLLFR', '(1, 0, S) FFRLF']
+
+const args = [grid, ...robots]
+
+const results = marsRover(...args)
 
 console.log('Final positions:', results)
